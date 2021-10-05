cc4f8c6ebfec6552a3c365e0de76c2effba2e67a
Orifha Mbedzi <9903125407086@psg.co.za>
2021-10-05 14:19:38 +0200

    Initial Commit

---
+++ resource-group-tagging.ps1
@@ -0,0 +1,19 @@
+﻿$rg = Get-AzResourceGroup -Name psgsan-ga-rg
+$tags = $rg.Tags;
+
+$Name = $rg.ResourceGroupName;
+$Description = Read-Host -Prompt "Description for $Name"
+$Purpose = Read-Host -Prompt "Purpose for $Name"
+$BusinessUnit = Read-Host "Business Unit for $Name (e.g Wealth)"
+$Environment = Read-Host "Environment for $Name (e.g development/production)"
+$CostCenter = Read-Host "Cost Center for $Name"
+
+
+$tags += @{Name = $Name }
+$tags += @{Description = $Description }
+$tags += @{Purpose = $Purpose }
+$tags += @{BusinessUnit = $BusinessUnit }
+$tags += @{Environment = $Environment }
+$tags += @{CostCenter = $CostCenter }
+
+Set-AzResourceGroup -Name psgsan-ga-rg -Tag $tags
---
+++ tagging.ps1
@@ -0,0 +1,37 @@
+﻿$rgName = Get-AzResourceGroup -Name psgsan-ga-rg
+$resources = Get-AzResource -ResourceGroupName psgsan-ga-rg
+foreach($resource in $resources){
+     $resourceName = $resource.Name 
+     if($resourceName -contains '*Failure*') {
+        continue;
+     }
+     $tags = $resource.Tags
+     $Description = Read-Host -Prompt "Description for $resourceName"
+     $Purpose = Read-Host -Prompt "Purpose for $resourceName"
+     $BusinessUnit = Read-Host -Prompt "BusinessUnit (e.g Wealth) for $resourceName"
+     $Application = Read-Host -Prompt "Application Type (e.g Jira) for $resourceName"
+     $AppComponent = Read-Host -Prompt "Appication Component (e.g Web/Db) for $resourceName"
+     $BusinessCriticality = Read-Host -Prompt "BusinessCriticality for $resourceName"
+     $DefaultAvailability = Read-Host -Prompt "DefaultCriticality for $resourceName"
+     $Environment = Read-Host -Prompt "Environment for $resourceName"
+     $BusinessOwner = Read-Host -Prompt "Business Owner for $resourceName"
+     $TechnicalOwner = Read-Host -Prompt "Technical Owner for $resourceName"
+     $CostCenter = Read-Host -Prompt "Cost Center for $resourceName"
+     $ResourceType = $resource.ResourceType;
+
+     $tags += @{Description=$Description }
+     $tags += @{Name=$resourceName }
+     $tags += @{Purpose=$Purpose }
+     $tags += @{BusinessUnit=$BusinessUnit }
+     $tags += @{Application=$Application }
+     $tags += @{AppComponent=$AppComponent }
+     $tags += @{BusinessCriticality=$BusinessCriticality }
+     $tags += @{DefaultAvailability=$DefaultAvailability }
+     $tags += @{Environment=$Environment }
+     $tags += @{BusinessOwner=$BusinessOwner }
+     $tags += @{TechnicalOwner=$TechnicalOwner }
+     $tags += @{CostCenter=$CostCenter }
+     $tags += @{ResourceType=$ResourceType}
+
+     Set-AzResource -ResourceId $resource.Id -Tag $tags
+}
